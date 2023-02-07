077203fe9d6fbab496778ec6449aecc390cf0075
AleksandrSharikov <Aleksandr.N.Sharikov@gmail.com>
2023-02-07 09:34:28 +0300

    Trying to make login menu

--- src/main/java/ru/kata/spring/boot_security/demo/controller/LoginController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/LoginController.java
@@ -4,16 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.processing.Generated;
-
 
 @Controller
 @RequestMapping(value = "/")
-public class DefaultController {
+public class LoginController {
 
-    @GetMapping(value = "/")
+
+   // public String rd() {        return "redirect:/login";    }
+  //  @GetMapping(value = "/")
+    @GetMapping(value = {"/login","/"})
     public String login() {
-        return "redirect:/login";
-    }
+        System.out.println("login");
+        return "login";}
 
 }
